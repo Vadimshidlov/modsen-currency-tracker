246c30b049586c1a058862792ca3e4740a968f15
Vadimshidlov <vadim.shidlov@gmail.com>
2024-03-09 13:45:11 +0300

    chore(CU-#86bxuj01z): remove unnecessary libraries

--- src/components/pages/TimelinePage/TimelinePage.tsx
+++ src/components/pages/TimelinePage/TimelinePage.tsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import Header from "@/components/pages/TimelinePage/Header/Header";
 import AppTitle from "@/components/pages/TimelinePage/AppTitle/AppTitle";
 import Footer from "@/components/pages/TimelinePage/Footer/Footer";
-import "react-toastify/dist/ReactToastify.css";
 import MainTimeComponent from "@/components/pages/TimelinePage/MainTimeComponent/MainTimeComponent";
 import { RootStateType } from "@/store/reducers";
 import { TimelinePagePropsType } from "@/types/TimeLinePageTypes/types";
